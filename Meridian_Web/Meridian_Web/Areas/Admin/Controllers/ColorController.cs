e9d805f8d49af83a207e0ac6cb3f91f79738a9b8
Ali <aliga@code.edu.az>
2023-03-01 16:28:07 +0400

    discount has been complated

---
+++ Meridian_Web/Meridian_Web/Areas/Admin/Controllers/ColorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Meridian_Web.Areas.Admin.Controllers
+{
+    public class ColorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
